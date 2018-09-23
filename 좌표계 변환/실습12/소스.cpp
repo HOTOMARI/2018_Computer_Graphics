94d2a1d598cbd650f068921c2f4114d8efd35af9
hogumin <hojiny9923@gmail.com>
2018-09-24 03:49:41 +0900

    실습 12

---
+++ 좌표계 변환/실습12/소스.cpp
@@ -0,0 +1,118 @@
+#include <GL/freeglut.h>
+#include<time.h>
+#include<math.h>
+#define PI 3.141592
+
+GLvoid DrawScene();
+GLvoid Reshape(int, int);
+GLvoid Mouse(int, int, int, int);
+GLvoid Keyboard(unsigned char, int, int);
+GLvoid SpecialKeyboard(int, int, int);
+GLvoid Timerfunction(int);
+
+
+int shape_mode = 0;	// 0 원 1 사인 2 회오리 3 지그재그 4 경로그리기
+
+int animation_speed = 1000;
+
+float moveX = 0, moveY = 0;
+float transformSize = 1;
+
+void main(int argc, char** argv) // 윈도우 출력하고 출력함수 설정 
+{
+	// 초기화 함수들 
+	glutInit(&argc, argv); // glut 초기화 
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); // 디스플레이 모드 설정 
+	glutInitWindowPosition(100, 100); // 윈도우의 위치 지정 
+	glutInitWindowSize(800, 600); // 윈도우의 크기 지정 
+	glutCreateWindow("Tiles"); // 윈도우 생성 (윈도우 이름) 
+	glutDisplayFunc(DrawScene); // 출력 함수의 지정 
+	glutReshapeFunc(Reshape); // 다시 그리기 함수 지정 
+	glutTimerFunc(1000 / 60, Timerfunction, 1);	// 타이머 셋팅
+	glutTimerFunc(animation_speed / 60, Timerfunction, 2);
+	glutMouseFunc(Mouse);		// 마우스 버튼 입력 받기
+	glutKeyboardFunc(Keyboard);	// 키보드 입력 받기
+	glutSpecialFunc(SpecialKeyboard);	// 키보드 특수버튼 입력 받기
+
+	srand(time(NULL));
+
+	glutMainLoop(); // 이벤트 처리 시작 
+}
+
+GLvoid DrawScene() // 출력 함수 
+{
+	glClearColor(0, 0, 0, 1); // 바탕색을 지정 
+	glClear(GL_COLOR_BUFFER_BIT); // 설정된 색으로 전체를 칠하기 
+
+	glPointSize(3.0);
+
+	// 중심선
+	glBegin(GL_LINES);
+	glVertex3f(0, 300, 0);
+	glVertex3f(0, -300, 0);
+	glVertex3f(400, 0, 0);
+	glVertex3f(-400, 0, 0);
+	glEnd();
+
+	
+
+
+	glutSwapBuffers(); // 화면에 출력하기 
+}
+
+GLvoid Reshape(int w, int h) // 다시 그리기 함수 
+{
+	glViewport(0, 0, w, h);
+	glOrtho(-400.0, 400.0, -300.0, 300.0, -400.0, 400.0);
+}
+
+GLvoid Mouse(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+	}
+	glutPostRedisplay();
+}
+
+GLvoid Keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 'q':
+	case 'Q':
+		glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_CONTINUE_EXECUTION);
+		glutLeaveMainLoop();
+		break;
+	case 'y':
+		moveY++;
+		break;
+	case 'Y':
+		moveY--;
+		break;
+	case 0:
+		break;
+	case '1':
+		break;
+	case '2':
+		break;
+	case '3':
+		break;
+	case '4':
+		break;
+	}
+	glutPostRedisplay();
+}
+
+GLvoid SpecialKeyboard(int key, int x, int y)
+{
+	switch (key) {
+
+	}
+}
+
+GLvoid Timerfunction(int value) {
+	switch (value) {
+	case 1:
+
+		glutPostRedisplay();
+		glutTimerFunc(animation_speed / 60, Timerfunction, 1);
+		break;
+	}
+}
