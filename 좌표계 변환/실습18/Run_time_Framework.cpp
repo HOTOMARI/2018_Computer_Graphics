3e7d067f1e19928ed15d12f861f227088d946018
hogumin_MyPC <hojiny9923@gmail.com>
2018-10-14 10:24:28 +0900

    18
    add reset

--- 좌표계 변환/실습18/Run_time_Framework.cpp
+++ 좌표계 변환/실습18/Run_time_Framework.cpp
@@ -17,7 +17,7 @@ GLvoid CRun_time_Framework::draw() {
 	glPushMatrix();
 	gluLookAt(0, 0, camera.zoom, 0.0, 0.0, -100.0, 0.0, 1.0, 0.0);
 	gluLookAt(camera.x, camera.y, 0, 0.0, 0.0, -100.0, 0.0, 1.0, 0.0);
-	gluLookAt(cos(camera.degree[2]/180*PI)*200, sin(camera.degree[2] / 180 * PI) * 200, 0, 0.0, 0.0, -100.0, 0.0, 1.0, 0.0);
+	gluLookAt(cos(camera.degree[2]/180*PI)*200 - 200, sin(camera.degree[2] / 180 * PI) * 200, 0, 0.0, 0.0, -100.0, 0.0, 1.0, 0.0);
 	gluLookAt(cos(camera.degree[1] / 180 * PI) * 200, 0, sin(camera.degree[1] / 180 * PI) * 200, 0.0, 0.0, -100.0, 0.0, 1.0, 0.0);
 	gluLookAt(0, sin(camera.degree[0] / 180 * PI) * 200, cos(camera.degree[0] / 180 * PI) * 200, 0.0, 0.0, -100.0, 0.0, 1.0, 0.0);
 	glMultMatrixf(identity);
@@ -214,6 +214,8 @@ GLvoid CRun_time_Framework::KeyboardDown(unsigned char key, int x, int y) {
 		camera.zoom = 0;
 		camera.x = 0;
 		camera.y = 0;
+		memset(identity, 0, sizeof(identity));
+		identity[0] = identity[5] = identity[10] = identity[15] = 1;
 		break;
 	case 'c':
 	case 'C':
